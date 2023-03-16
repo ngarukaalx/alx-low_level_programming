dfb91cc2e1fa6164347911a5cc2045da6a3f65fe
ngarukaalx <hiramkabuagi@gmail.com>
2023-03-16 12:38:34 -0700

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,17 @@
- #include <stdio.h>
- 
- /**
-  * main - Entry point
-  *
-  * Return:Always 0 (Success)
-  */
- int main(void)
- {
- 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
- }	
+ 1 /*
+ 2  * File: 4-puts.c
+ 3  * HELLO:
+ 4  */
+ 5
+ 6 #include <stdio.h>
+ 7 
+ 8 /**
+ 9  * main - Entry point
+10  *
+11  * Return:Always 0 (Success)
+12  */
+13 int main(void)
+14 {
+15 	puts("\"Programming is like building a multilingual puzzle");
+16	return (0);
+17 }	
