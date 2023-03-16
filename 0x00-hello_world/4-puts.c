77c6128debeb17428a4a5936960155ebd0ef34d6
ngarukaalx <hiramkabuagi@gmail.com>
2023-03-16 12:50:59 -0700

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,20 @@
- 1 /*
- 2  * File: 4-puts.c
- 3  * HELLO:
- 4  */
- 5
- 6 #include <stdio.h>
- 7 
- 8 /**
- 9  * main - Entry point
-10  *
-11  * Return:Always 0 (Success)
-12  */
-13 int main(void)
-14 {
-15 	puts("\"Programming is like building a multilingual puzzle");
-16	return (0);
-17 }	
+
+/*
+ * File: 4-puts.c
+ * HIRAM: kabuagi
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
