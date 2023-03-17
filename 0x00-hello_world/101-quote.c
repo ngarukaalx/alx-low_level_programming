bddddd38c4d647a7870228643d79e20e4fadca62
ngarukaalx <hiramkabuagi@gmail.com>
2023-03-17 00:58:43 -0700

    g

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ *
+ * returns:Always 1.
+ */
+
+int main(void)
+{
+	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, message, sizeof(message) - 1);
+	return (1);
+}
