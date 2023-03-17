a5ab33b9109a885b5505e9f74bd3812fdfde8ee4
ngarukaalx <hiramkabuagi@gmail.com>
2023-03-17 01:29:46 -0700

    betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,7 @@
 
 int main(void)
 {
-	const char message[] =
-	       	"and that piece of art is useful\"i - Dora Korpar, 2015-10-19\n";
+	char message[] = "and that piece of art is useful\"i - Dora Korpar, 2015-10-19\n";
 	write(2, message, sizeof(message) - 1);
 
 	return (1);
