c4f09425af33d197e69116fde319613a309a27e4
ngarukaalx <hiramkabuagi@gmail.com>
2023-03-18 09:06:53 -0700

    g

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -Determine if a random number is positive, negative or zero.
+ *
+ * Return:returns 0
+ */
+
+int main(void)
+{
+	char k;
+
+	for (k = 'z'; k >= 'a'; k--)
+	{
+		putchar(k);
+	}
+	putchar('\n');
+	return (0);
+}
