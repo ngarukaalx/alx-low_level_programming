3ed5bdb04eaa402374d0c789027d93d523345fe5
ngarukaalx <hiramkabuagi@gmail.com>
2023-03-18 06:47:27 -0700

    g

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -Determine if a random number is positive, negative or zero.
+ *
+ * Return:returns 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	return (0);
+}
