d79847e09f9a9422d6f6e17224d85692f5475bec
ngarukaalx <hiramkabuagi@gmail.com>
2023-03-19 04:45:27 -0700

    three

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main -prints possible combination of three digits number.
+ *
+ * Return:returns 0
+ */
+
+int main(void)
+{
+	int a, b, c;
+
+	for (a = '0'; a < '8'; a++)
+	{
+		for (b = a + 1; b < '9'; b++)
+		{
+			for (c = b + 1; c < 10; c++)
+			{
+				if (a != b && a != c && c != b)
+				{
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(c + '0');
+					if (a + b + c < 24)
+					{
+					putchar(',');
+					putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
