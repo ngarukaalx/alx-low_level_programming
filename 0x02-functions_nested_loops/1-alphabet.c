70d4d74ff9d5abfc0df62c89f15bdec0657215bf
ngarukaalx <hiramkabuagi@gmail.com>
2023-03-21 06:17:14 -0700

    print a-z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ *
+ * Return:returns 0
+ */
+
+int main(void)
+{
+	char k;
+
+	for (k = 'a'; k <= 'z'; k++)
+	{
+		putchar(k);
+		}
+	putchar('\n');
+	return (0);
+}
