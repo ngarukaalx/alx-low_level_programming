f95d6e8b644dd2cb64ea5aa3aada00bf1b1dd50b
ngarukaalx <hiramkabuagi@gmail.com>
2023-03-21 05:23:53 -0700

    prints putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -Determine if a random number is positive, negative or zero.
+ *
+ * Return:returns 0
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
