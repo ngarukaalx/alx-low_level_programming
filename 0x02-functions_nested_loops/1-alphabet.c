22897fc691215df7ce7789948517a3ee03494a13
ngarukaalx <hiramkabuagi@gmail.com>
2023-03-22 02:07:24 -0700

    a-z

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,12 +3,7 @@
 /**
  * print_alphabet - prints the alphabet in lowercase, followed by a new line.
  */
-void print_alphabet(void);
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
+
 void print_alphabet(void)
 {
 	char k;
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ */
+void print_alphabet(void);
+int main(void)
+{
+        print_alphabet();
+        return (0);
+}
