0951bc32ef02591fb1b058b7b55d808742cd50fb
ngarukaalx <hiramkabuagi@gmail.com>
2023-03-25 05:30:10 -0700

    1 to 100

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints 1 to 100.
+ *
+ * return:return 1 t0 100 on sucuses
+ */
+
+int main()
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0 && a % 5 == 0)
+			printf("FizzBuzz");
+		else if (a % 3 == 0)
+			printf("Fizz");
+		else if (a % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", a);
+		{
+			printf(" ");
+		}
+	}
+	putchar('\n');
+	return (0);
+}
