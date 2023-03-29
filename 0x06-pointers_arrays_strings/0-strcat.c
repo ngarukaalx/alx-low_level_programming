77a479839b157b7532246a33f220f3cb1d808690
ngarukaalx <hiramkabuagi@gmail.com>
2023-03-29 09:50:36 -0700

    concatenates

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,7 +10,16 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	strcat(dest, " ");
-	strcat(dest, src);
+	char *temp = dest;
+
+	while (*temp != '\0')
+	{
+		temp++;
+	}
+	while (*src != '\0')
+	{
+		*temp++ = *src++;
+	}
+	*temp = '\0';
 	return (dest);
 }
