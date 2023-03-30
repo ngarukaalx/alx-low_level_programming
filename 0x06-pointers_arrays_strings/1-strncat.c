e756a330d34e219cd8220dc425dbfd497d81e5ea
ngarukaalx <hiramkabuagi@gmail.com>
2023-03-30 02:00:28 -0700

    concatenates

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strncat -  concatenates two strings.
+ * @dest:being checked
+ * @src:being checked
+ *
+ * Return:Always 0.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest, src, n);
+
+	return (dest);
+}
