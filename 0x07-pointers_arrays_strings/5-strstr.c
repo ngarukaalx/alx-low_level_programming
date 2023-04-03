070a79c32b03b22f0e369f1d3f7f4729f57fe5e9
ngarukaalx <hiramkabuagi@gmail.com>
2023-04-03 09:16:21 -0700

    locates

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strstr -  locates a substring.
+ * @haystack:being checked.
+ * @needle:being checked.
+ *
+ * Return:returns a pointer to the beginning of the located
+ * substring, or NULL if the substring is not found.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	return (strstr(haystack, needle));
+}
