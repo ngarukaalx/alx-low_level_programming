6ba76700b660c8b6a2029f2c5b27135c98828bbb
ngarukaalx <hiramkabuagi@gmail.com>
2023-04-14 13:26:50 -0700

    rellocates a memory block

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _realloc - reallocates a memory block using malloc and free.
+ * @old_size:old size
+ * @new_size:new size
+ * ptr:ptr to old size
+ *
+ * Return:pointer to new size
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *ptrr;
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	ptrr = malloc(new_size);
+	if (ptrr == NULL)
+	{
+		return (NULL);
+	}
+	if (ptrr != NULL)
+	{
+		memcpy(ptrr, ptr, old_size < new_size ? old_size : new_size);
+		free(ptr);
+	}
+	return (ptrr);
+
+}
