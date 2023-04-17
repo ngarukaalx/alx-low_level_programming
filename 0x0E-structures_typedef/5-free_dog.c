858b5ab2c32e1d73345b172f52e008a5e1095a70
ngarukaalx <hiramkabuagi@gmail.com>
2023-04-17 14:39:24 -0700

    free

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -10,9 +10,9 @@ void free_dog(dog_t *d)
 {
 	if (d == NULL)
 		return;
-	if (d->name == NULL)
+	if (d->name != NULL)
 		free(d->name);
-	if (d->owner == NULL)
+	if (d->owner != NULL)
 		free(d->owner);
 	free(d);
 }
