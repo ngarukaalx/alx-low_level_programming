0fbac3b314ab9c1aeab222152d0f98980c84f208
ngarukaalx <hiramkabuagi@gmail.com>
2023-05-11 02:43:56 -0700

    elf file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -72,18 +72,18 @@ void print_elf_header(Elf64_Ehdr *ehdr)
 	printf("Magic:");
 	for (i = 0; i < EI_NIDENT; i++)
 	{
-		printf("%02x", ehdr->e_ident[i]);
+		printf("%02x ", ehdr->e_ident[i]);
 	}
 	printf("\n");
 	printf("  Class:		%s\n", (ehdr->e_ident[EI_CLASS] == ELFCLASS64) ?
 			"ELF64" : "ELF32");
 	printf("  Data:			%s\n", (ehdr->e_ident[EI_DATA] == ELFDATA2LSB) ?
-			"little-endian" : "big-endian");
-	printf("  Version:		%d\n", ehdr->e_ident[EI_VERSION]);
-	printf("  OS/ABI:		%d\n", ehdr->e_ident[EI_OSABI]);
-	printf("  ABI Version:		%d\n", ehdr->e_ident[EI_ABIVERSION]);
-	printf("  Type:			%d\n", ehdr->e_type);
-	printf("  Entry point address:	0x%lx\n", ehdr->e_entry);
+			"2's complement, little-endian" : "2's complement, big-endian");
+	printf("  Version:		%u\n", ehdr->e_ident[EI_VERSION]);
+	printf("  OS/ABI:		%u\n", ehdr->e_ident[EI_OSABI]);
+	printf("  ABI Version:		%u\n", ehdr->e_ident[EI_ABIVERSION]);
+	printf("  Type:			%u\n", ehdr->e_type);
+	printf("  Entry point address:	%#010lx\n", ehdr->e_entry);
 }
 
 /**
