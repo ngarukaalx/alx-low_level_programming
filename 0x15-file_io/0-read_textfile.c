cb883fe51a6122bf435f865e5dfc4a3ef0250b9f
ngarukaalx <hiramkabuagi@gmail.com>
2023-07-10 03:32:13 -0700

    file i/o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,45 +11,41 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file_descriptor;
+	int fd;
 
 	char *buffer;
 
 	ssize_t bytes_read, bytes_written;
 
 	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 	{
 		return (0);
 	}
-
-	file_descriptor = open(filename, O_RDONLY);
-	if (file_descriptor == -1)
-	{
-		perror("cannot open file");
-		return (0);
-	}
-	buffer = (char *)malloc(sizeof(char) * letters);
+	buffer = (char *) malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
-		close(file_descriptor);
+		close(fd);
 		return (0);
 	}
-	bytes_read = read(file_descriptor, buffer, letters);
+	bytes_read = read(fd, buffer, letters);
 	if (bytes_read == -1)
 	{
 		free(buffer);
-		close(file_descriptor);
+		close(fd);
 		return (0);
 	}
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 	if (bytes_written == -1 || bytes_written != bytes_read)
 	{
 		free(buffer);
-		close(file_descriptor);
+		close(fd);
 		return (0);
 	}
 	free(buffer);
-	close(file_descriptor);
-
+	close(fd);
 	return (bytes_written);
+
 }
