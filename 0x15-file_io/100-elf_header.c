2df059cb7fb6da308ab74f55f1c8c27935ace09e
ngarukaalx <hiramkabuagi@gmail.com>
2023-07-10 11:33:26 -0700

    elf

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <elf.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+Elf64_Ehdr *openElfFile(char *elf_filename)
+{
+    int fd;
+    Elf64_Ehdr *elf;
+
+    fd = open(elf_filename, O_RDONLY);
+    if (fd == -1)
+    {
+        perror("Error opening file");
+        exit(98);
+    }
+
+    elf = malloc(sizeof(Elf64_Ehdr));
+    if (elf == NULL)
+    {
+        perror("Error allocating memory");
+        exit(98);
+    }
+
+    if (read(fd, elf, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr))
+    {
+        perror("Error reading file");
+        free(elf);
+        exit(98);
+    }
+
+    close(fd);
+    return elf;
+}
+
+Elf64_Ehdr *get_elf_header(Elf64_Ehdr *elf)
+{
+    if (elf->e_ident[EI_MAG0] != ELFMAG0 ||
+        elf->e_ident[EI_MAG1] != ELFMAG1 ||
+        elf->e_ident[EI_MAG2] != ELFMAG2 ||
+        elf->e_ident[EI_MAG3] != ELFMAG3)
+    {
+        fprintf(stderr, "Error: Not an ELF file\n");
+        exit(98);
+    }
+
+    return elf;
+}
+
+void print_elf_header(Elf64_Ehdr *ehdr)
+{
+    int i;
+
+    printf("ELF Header:\n");
+    printf("  Magic:  ");
+    for (i = 0; i < EI_NIDENT; i++)
+    {
+        printf("%02x ", ehdr->e_ident[i]);
+    }
+    printf("\n");
+    printf("  Class:                          %s\n", (ehdr->e_ident[EI_CLASS] == ELFCLASS64) ? "ELF64" : "ELF32");
+    printf("  Data:                           %s\n", (ehdr->e_ident[EI_DATA] == ELFDATA2LSB) ? "2's complement, little-endian" : "2's complement, big-endian");
+    printf("  Version:                        %d (current)\n", ehdr->e_ident[EI_VERSION]);
+    printf("  OS/ABI:                         %d\n", ehdr->e_ident[EI_OSABI]);
+    printf("  ABI Version:                    %d\n", ehdr->e_ident[EI_ABIVERSION]);
+    printf("  Type:                           %d\n", ehdr->e_type);
+    printf("  Entry point address:            %#010lx\n", ehdr->e_entry);
+}
+
+int main(int argc, char *argv[])
+{
+    Elf64_Ehdr *elf;
+    Elf64_Ehdr *ehdr;
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
+        exit(98);
+    }
+
+    elf = openElfFile(argv[1]);
+    ehdr = get_elf_header(elf);
+    print_elf_header(ehdr);
+
+    free(elf);
+    return 0;
+}
+
