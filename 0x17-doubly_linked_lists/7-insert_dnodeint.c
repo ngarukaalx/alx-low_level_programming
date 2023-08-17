14039a0c157b2e713a6b85313c2c8a8b9a453719
ngarukaalx <hiramkabuagi@gmail.com>
2023-08-17 13:30:21 +0000

    add at given index

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,75 @@
+#include "lists.h"
+
+/**
+ * creat_node - fuction name
+ * @n: parameter data
+ *
+ * Return: &newnode
+ */
+
+dlistint_t *creat_node(int n)
+{
+	dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+
+	return (new_node);
+}
+
+/**
+ * insert_dnodeint_at_index - fuction name
+ * @h: head ponter
+ * @idx: index toinsert
+ * @n: data
+ *
+ * Return: &newnode
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int count = 0;
+
+	dlistint_t *new_node = creat_node(n);
+
+	dlistint_t *current = *h;
+
+	if (idx == 0)
+	{
+		new_node->next = *h;
+		if (*h != NULL)
+		{
+			(*h)->prev = new_node;
+		}
+		*h = new_node;
+		return (new_node);
+	}
+
+	while (current != NULL && count < idx - 1)
+	{
+		current = current->next;
+		count++;
+	}
+
+	if (current == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = current->next;
+	new_node->prev = current;
+
+	if (current->next != NULL)
+	{
+		current->next->prev = new_node;
+	}
+
+	current->next = new_node;
+	return (new_node);
+}
