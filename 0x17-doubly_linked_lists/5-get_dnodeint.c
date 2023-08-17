5881671f12ae88e9d5a960ec2c68518b1ec756ce
ngarukaalx <hiramkabuagi@gmail.com>
2023-08-17 12:13:09 +0000

    nthnode

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -14,11 +14,6 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	unsigned int i = 0;
 
-	if (index < 1)
-	{
-		return (NULL);
-	}
-
 	while (current != NULL && i < index)
 	{
 		current = current->next;
