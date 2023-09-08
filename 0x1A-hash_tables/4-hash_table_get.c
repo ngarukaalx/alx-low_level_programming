72f9ce995b26be54e55865710ce23455e6541373
ngarukaalx <hiramkabuagi@gmail.com>
2023-09-08 18:44:20 +0000

    return var at index

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - function that retrieves a value associated with a key.
+ * @ht: hash table you want to look into
+ * @key: key you are looking for
+ *
+ * Return: value associated with the element, or NULL if key
+ * couldn’t be found
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+
+	hash_node_t *item;
+
+	if (ht == NULL || key == NULL)
+	{
+		return (NULL);
+	}
+
+	index = key_index(( const unsigned char *)key, ht->size);
+
+	item = ht->array[index];
+	
+	if (item != NULL)
+	{
+		if (strcmp(item->key, key) == 0)
+			return (item->value);
+		item = item->next;
+	}
+	return (NULL);
+
+}
