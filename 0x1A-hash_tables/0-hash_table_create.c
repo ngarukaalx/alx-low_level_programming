dadfff6b7a3e748e05fdf051a7103965a597dfb8
ngarukaalx <hiramkabuagi@gmail.com>
2023-09-10 11:08:51 +0000

    fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -21,7 +21,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table->array = (hash_node_t **)calloc(table->size, sizeof(hash_node_t *));
 	if (table->array == NULL)
 	{
-		free (table);
+		free(table);
 		return (NULL);
 	}
 
